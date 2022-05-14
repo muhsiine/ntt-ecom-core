170fb643dcc63b09a0b637cd92f9418e246ca66d
muhsiine <muhsiine@gmail.com>
2022-05-14 18:57:40 +0100

    add test that would fail;

--- src/test/java/ma/nttsquad/nttecomcore/NttEcomCoreApplicationTests.java
+++ src/test/java/ma/nttsquad/nttecomcore/NttEcomCoreApplicationTests.java
@@ -3,11 +3,14 @@ package ma.nttsquad.nttecomcore;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @SpringBootTest
 class NttEcomCoreApplicationTests {
 
     @Test
     void contextLoads() {
+        assertNotNull(null);
     }
 
 }
