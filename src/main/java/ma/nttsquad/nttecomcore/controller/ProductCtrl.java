21c4dfec81910a82727792aa9418a538103388a9
muhsiine <muhsiine@gmail.com>
2022-05-29 17:26:50 +0100

    feature/add-controller-advice: format code.

--- src/main/java/ma/nttsquad/nttecomcore/controller/ProductCtrl.java
+++ src/main/java/ma/nttsquad/nttecomcore/controller/ProductCtrl.java
@@ -9,7 +9,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import ma.nttsquad.nttecomcore.exception.NttNotFoundException;
-import ma.nttsquad.nttecomcore.model.Category;
 import ma.nttsquad.nttecomcore.model.Product;
 import ma.nttsquad.nttecomcore.repository.ProductRepository;
 import org.springframework.web.bind.annotation.*;
@@ -33,8 +32,8 @@ public class ProductCtrl {
             @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(array = @ArraySchema(schema = @Schema(implementation = Product.class)))),
             @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(schema = @Schema(description = "HTTP status error code", example = "400")))
     })
-    @GetMapping(value={"/all"})
-    public List<Product> getAllProducts(){
+    @GetMapping(value = {"/all"})
+    public List<Product> getAllProducts() {
         return productRepository.findAll();
     }
 
@@ -44,10 +43,10 @@ public class ProductCtrl {
             @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(schema = @Schema(description = "HTTP status error code", example = "400")))
     })
     @GetMapping("/{id}")
-    public Product getProductById(@PathVariable("id") Long productId){
+    public Product getProductById(@PathVariable("id") Long productId) {
         log.info("Id: {}", productId);
         return productRepository.findById(productId)
-                .orElseThrow( () -> new NttNotFoundException("Product with id: '%d' not found!".formatted(productId)));
+                .orElseThrow(() -> new NttNotFoundException("Product with id: '%d' not found!".formatted(productId)));
     }
 
     @Operation(summary = "Find Product by Category Id", description = "Find Product by Category Id", tags = "Product")
@@ -56,7 +55,7 @@ public class ProductCtrl {
             @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(schema = @Schema(description = "HTTP status error code", example = "400")))
     })
     @GetMapping("/category/{id}")
-    public List<Product> getProductByCategoryId(@PathVariable("id") Long categoryId){
+    public List<Product> getProductByCategoryId(@PathVariable("id") Long categoryId) {
         return productRepository.findByCategoryId(categoryId);
     }
 
@@ -66,7 +65,7 @@ public class ProductCtrl {
             @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(schema = @Schema(description = "HTTP status error code", example = "400")))
     })
     @PostMapping("/save")
-    public void saveProduct(@RequestBody Product product){
+    public void saveProduct(@RequestBody Product product) {
         productRepository.save(product);
     }
 
--- src/main/java/ma/nttsquad/nttecomcore/exception/NttNotFoundException.java
+++ src/main/java/ma/nttsquad/nttecomcore/exception/NttNotFoundException.java
@@ -1,12 +1,9 @@
 package ma.nttsquad.nttecomcore.exception;
 
-public class NttNotFoundException extends RuntimeException{
+public class NttNotFoundException extends RuntimeException {
 
     public NttNotFoundException(String message) {
         super(message);
     }
 
-    public NttNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
 }
