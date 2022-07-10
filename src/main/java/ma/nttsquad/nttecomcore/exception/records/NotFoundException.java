7dedd77e806c4119954dbbd1b915a46c0032506b
Yassir El Reklaoui <yelrekla@everis.com>
2022-07-10 18:36:47 +0100

    change the naming

--- src/main/java/ma/nttsquad/nttecomcore/exception/records/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ma.nttsquad.nttecomcore.exception.records;
-
-import org.springframework.http.HttpStatus;
-
-import java.time.LocalDateTime;
-
-public record NotFoundException(String message, HttpStatus httpStatus, LocalDateTime timeStamp) {}
